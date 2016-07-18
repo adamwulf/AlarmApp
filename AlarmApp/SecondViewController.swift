532af1b53b780dd8e92401026889de11ecc8f8f9
kk6rpt <grace.ding@warriorlife.net>
2016-07-18 10:18:28 -0700

    First commit

--- AlarmApp/SecondViewController.swift
+++ AlarmApp/SecondViewController.swift
@@ -7,19 +7,98 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var btnRecord: UIButton!
+    
+    @IBOutlet weak var btnPlay: UIButton!
+    
+    var audioRecorder : AVAudioRecorder!
+    var audioPlayer : AVAudioPlayer!
+    
+    let isRecorderAudioFile = false
+    let recordSettings = [AVSampleRateKey : NSNumber(float: Float(44100.0)),AVFormatIDKey : NSNumber(int: Int32(kAudioFormatMPEG4AAC)),AVNumberOfChannelsKey : NSNumber(int: 1),
+                          AVEncoderAudioQualityKey : NSNumber(int: Int32(AVAudioQuality.Medium.rawValue))]
+    
+    func directoryURL() -> NSURL? {
+        let fileManager = NSFileManager.defaultManager()
+        let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        let documentDirectory = urls[0] as NSURL
+        let soundURL = documentDirectory.URLByAppendingPathComponent("sound.m4a")
+        print(soundURL)
+        return soundURL
+    }
+    
+    
+    @IBAction func doRecording(sender: AnyObject) {
+        if sender.titleLabel!!.text == "Record" {
+            let audioSession = AVAudioSession.sharedInstance()
+            do {
+                try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+                try audioRecorder = AVAudioRecorder(URL: self.directoryURL()!,
+                                                    settings: recordSettings)
+                audioRecorder.prepareToRecord()
+            } catch {
+            }
+            do {
+                self.btnRecord.setTitle("Stop", forState: UIControlState.Normal)
+                self.btnPlay.enabled = false
+                try audioSession.setActive(true)
+                audioRecorder.record()
+            } catch {
+            }
+        }else{
+            audioRecorder?.stop()
+            let audioSession = AVAudioSession.sharedInstance()
+            do {
+                self.btnRecord?.setTitle("Record", forState: UIControlState.Normal)
+                self.btnPlay.enabled = true
+                try audioSession.setActive(false)
+            } catch {
+            }
+        }
+    }
+    
+    
+    @IBAction func doPlay(sender: AnyObject) {
+        if !audioRecorder.recording {
+            self.audioPlayer = try! AVAudioPlayer(contentsOfURL: audioRecorder.url)
+            self.audioPlayer.prepareToPlay()
+            self.audioPlayer.delegate = self.audioPlayer.delegate
+            self.audioPlayer.play()
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        print(flag)
+    }
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+        print(flag)
+    }
+    func audioPlayerDecodeErrorDidOccur(player: AVAudioPlayer, error: NSError?){
+        print(error.debugDescription)
+    }
+    internal func audioPlayerBeginInterruption(player: AVAudioPlayer){
+        print(player.debugDescription)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        //self.btnPlay.enabled = false
+        
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    }
 
-
-}
 
---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'AlarmApp' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AlarmApp
+	pod 'DZNEmptyDataSet'
+	pod ‘Alamofire’
+	pod ‘SnapKit’
+	pod ‘Parse’
+
+  target 'AlarmAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'AlarmAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
+
